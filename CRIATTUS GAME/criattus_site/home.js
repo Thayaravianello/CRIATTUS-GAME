c5dd256cee39bd5f18194c031d5f0f4a11857114
Thayara Vianello <thayaraferreira@alunos.unicesumar.edu.br>
2024-01-06 18:00:55 +0100

    Subindo projetos

---
+++ CRIATTUS GAME/criattus_site/home.js
@@ -0,0 +1,32 @@
+function clicouniki(){
+if(document.getElementById("mesa1").style.display === "flex")
+document.getElementById("mesa1").style.display = "none";
+else
+    document.getElementById("mesa1").style.display = "flex";
+}
+
+
+
+function clicoutay(){
+    if(document.getElementById("mesa2").style.display === "flex")
+    document.getElementById("mesa2").style.display = "none";
+    else
+        document.getElementById("mesa2").style.display = "flex";
+    }
+
+ 
+    function closed(){
+        if(document.getElementById("mesa1").style.display === "none")
+        document.getElementById("mesa1").style.display = "flex";
+        else
+            document.getElementById("mesa1").style.display = "none";
+        }
+
+
+        function closed2(){
+            if(document.getElementById("mesa2").style.display === "none")
+            document.getElementById("mesa2").style.display = "flex";
+            else
+                document.getElementById("mesa2").style.display = "none";
+            }
+        
